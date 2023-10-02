00b27bf58842fc222857ad41f48d2aaa21c67dd0
Sukuna18 <gossfall10@gmail.com>
2023-10-02 14:48:39 +0000

    feat:

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -106,7 +106,7 @@ const Navbar = (): ReactElement => {
             </a>
           );
         })}
-        <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+        <div id="theme" className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
           <input
             type="checkbox"
             name="toggle"
@@ -120,41 +120,6 @@ const Navbar = (): ReactElement => {
           ></label>
         </div>
       </nav>
-
-<nav id="burger" className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <button data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-      </svg>
-    </button>
-    <div className="hidden w-full" id="navbar-hamburger">
-      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-        <li>
-          <a
-          href="#home"
-          className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a 
-          href="#about"
-           className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">About</a>
-        </li>
-        <li>
-          <a 
-          href="#resume"
-          className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Resume</a>
-        </li>
-        <li>
-          <a 
-          href="#projects"
-           className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Projects</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
     </div>
   );
