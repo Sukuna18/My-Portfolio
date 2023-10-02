872269ee1e0d4742490c9d9b9e1cf87b5fb2f418
Sukuna18 <gossfall10@gmail.com>
2023-10-02 14:41:55 +0000

    fix: :rotating_light:

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/flowbite/**/*.js"
+  ],
+  plugins: [
+    
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin')
   ],
   darkMode: 'class',
 }
